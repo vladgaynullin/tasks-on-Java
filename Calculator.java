78b9a65710be73272604a3bf020fd63f86a4d486
vladgaynullin <vladislavgaynullin@gmail.com>
2022-08-28 18:49:38 +0400

    Calc from file (added output to file)

--- Calculator.java
+++ Calculator.java
@@ -1,15 +1,19 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 // Обработка исключений: нет файла, не число, не верный знак вычислений, деление на 0
 public class Calculator {
-    public static void main(String[] args) {
-        String path = "calc_input";
-        File file = new File(path);
+    public static void main(String[] args) throws FileNotFoundException {
+        String path_in = "calc_input";
+        String path_out = "calc_output";
+        File file_in = new File(path_in);
+        File file_out = new File(path_out);
+        PrintStream ps = new PrintStream(file_out);
         try {
-            Scanner sc = new Scanner(file);
+            Scanner sc = new Scanner(file_in);
             while (sc.hasNextLine()) {
                 try {
                     String[] symbols = sc.nextLine().split(" ");
@@ -22,6 +26,7 @@ public class Calculator {
                                 throw new IOException();
                             } catch (IOException e) {
                                 System.out.println("Operation Error!");
+                                ps.println("Operation Error!");
                             }
                         } else {
                             if (k.equals("/")) {
@@ -30,23 +35,30 @@ public class Calculator {
                                         throw new IOException();
                                     } catch (IOException e) {
                                         System.out.println("Error! Division by zero");
+                                        ps.println("Error! Division by zero");
                                     }
                                 } else {
                                     System.out.println(a / b);
+                                    ps.println(a / b);
                                 }
                             } else if (k.equals("+")) {
                                 System.out.println(a + b);
+                                ps.println(a + b);
                             } else if (k.equals("-")) {
                                 System.out.println(a - b);
+                                ps.println(a - b);
                             } else if (k.equals("*")) {
                                 System.out.println(a * b);
+                                ps.println(a * b);
                             }
                         }
                     } catch (Exception e) {
                         System.out.println("Operation Error!");
+                        ps.println("Operation Error!");
                     }
                 } catch (NumberFormatException e) {
                     System.out.println("Error! Not number");
+                    ps.println("Error! Not number");
                 }
             }
         } catch (FileNotFoundException e) {
